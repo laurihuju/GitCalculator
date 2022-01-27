c8ede4b102f1bfc1229e0311f85e374db839afe2
Huju Lauri <s2000949@edu.bc.fi>
2022-01-27 12:32:19 +0200

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+int calculation = getNumber("Kirjoita numero 1:") + getNumber("Kirjoita numero 2:");
+Console.WriteLine(calculation);
+
+int getNumber(string text)
+{
+    try
+    {
+        Console.WriteLine(text);
+        int number = Int32.Parse(Console.ReadLine());
+        return number;
+    }
+    catch(FormatException)
+    {
+        Console.WriteLine("Antamasi syöte ei ole numero!");
+        return getNumber(text);
+    }
+}
