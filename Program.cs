e93b8de1cec15a9b178f3b2e2907c7064d419f22
Huju Lauri <s2000949@edu.bc.fi>
2022-02-03 12:46:14 +0200

    Lisätty yhteenlaskun lisäksi vähennyslaskun laskeminen.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
 ﻿using System;
 
-int calculation = getNumber("Kirjoita numero 1:") + getNumber("Kirjoita numero 2:");
-Console.WriteLine(calculation);
+/*string operation = readOperation();
+int calculation = 0;
+if(operation == "+")
+{
+    calculation = getNumber("Kirjoita numero 1:") + getNumber("Kirjoita numero 2:");
+} else if(operation == "-")
+{
+    calculation = getNumber("Kirjoita numero 1:") - getNumber("Kirjoita numero 2:");
+} else if(operation == "*")
+{
+    calculation = getNumber("Kirjoita numero 1:") * getNumber("Kirjoita numero 2:");
+} else if(operation == "/")
+{
+    calculation = getNumber("Kirjoita numero 1:") / getNumber("Kirjoita numero 2:");
+}*/
+int number1 = getNumber("Kirjoita numero 1:");
+int number2 = getNumber("Kirjoita numero 2:");
+int summa = number1 + number2;
+int erotus = number1 - number2;
+Console.WriteLine("summa = " + summa);
+Console.WriteLine("erotus = " + erotus);
 
 int getNumber(string text)
 {
@@ -16,4 +35,16 @@ int getNumber(string text)
         Console.WriteLine("Antamasi syöte ei ole numero!");
         return getNumber(text);
     }
+}
+
+string readOperation()
+{
+    Console.WriteLine("Kirjoita suoritettava laskuoperaatio:");
+    String operation = Console.ReadLine();
+    if(operation == "+" || operation == "-" || operation == "*" || operation == "/")
+        return operation;
+
+    Console.WriteLine("Laskuoperaatiota " + operation + " ei tunnistettu!");
+    Console.WriteLine();
+    return readOperation();
 }
